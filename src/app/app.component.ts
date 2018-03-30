526c79d09037418ab5e77186caf71f5338c08bcc
surendrasharma0602 <surendrasharma0602@gmail.com>
2018-03-30 11:40:43 +0530

    added fb connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private facebookAuthEndpoint = 'https://www.facebook.com/v2.10/dialog/oauth';
+  private responseType = 'token';
+  private clientId = '';
+  private redirectUrl = 'http://travelbuddyapp.com:8080/callback.html';
+  private scope = 'public_profile user_friends';
   title = 'My First Angular Application';
+  constructor(private httpClient: HttpClient) {
+    // tslint:disable-next-line:max-line-length
+    httpClient.get('https://graph.facebook.com/oauth/access_token?response_type=token&client_id=&state=xyz&redirect_uri=https://localhost:4200/')
+      .subscribe(data => console.log(data));
+  }
 }
